327ba156449c4a7e34c6ab948826b8b049a0418d
Alissa Balge <alissa.balge@gmail.com>
2018-11-06 19:40:01 +0000

    completed ex 8

--- lib/employee.rb
+++ lib/employee.rb
@@ -4,4 +4,12 @@ class Employee < ActiveRecord::Base
   validates :last_name, presence: true
   validates :hourly_rate, numericality: {only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
   validates :store_id, presence: true
+
+  before_save :create_password
+
+  private
+    def create_password
+    self.password = (0...8).map { (65 + rand(26)).chr }.join
+  end
+
 end
